bb7ca63c67befc546b4576b1d411093ef13c1661
mateushzet <mateushzett@gmail.com>
2022-11-20 16:28:35 +0100

    3

--- src/CallableCalculator.java
+++ src/CallableCalculator.java
@@ -1,46 +1,48 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
 public class CallableCalculator implements Callable {
 
-
+    static ArrayList<Boolean> reservationLine = new ArrayList<Boolean>();
 
 
    FileInputStream f;
 
-     {
+    {
         try {
             f = new FileInputStream("C:\\Users\\mateu\\IdeaProjects\\Concurrent_file_read6\\src\\file.txt");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
+
     DataInputStream in = new DataInputStream(f);
-     BufferedReader r = new BufferedReader(new InputStreamReader(in));
+   BufferedReader r = new BufferedReader(new InputStreamReader(in));
 
     public Object call() throws Exception {
+        int lineNumber = 0;
         String strLine = new String();
         Boolean isCalculated = false;
-        String allResults = new String();
         String output = new String();
-        while ((strLine = r.readLine()) != null && !isCalculated) {{
+        while ((strLine = r.readLine()) != null && !isCalculated) {
             strLine.trim();
             int indexOfEquals = strLine.indexOf('=');
             if(strLine.length() == indexOfEquals+1){
 
-
+                reservationLine
                 strLine = strLine.replaceAll("=","");
                 ONP calculator = new ONP(strLine);
                 output += calculator.oblicz();
-                allResults += strLine+" = "+output+"\t"+Thread.currentThread().getName()+System.lineSeparator();
                 System.out.println(Thread.currentThread().getName() + "  -  "+ strLine);
                 isCalculated = true;
 
-            } else strLine = r.readLine();}
+
+            }else lineNumber++;
         }
         in.close();
-        return allResults;
+        return output;
     }
 
 }
--- src/MyFutureTask.java
+++ src/MyFutureTask.java
@@ -1,6 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -16,18 +16,75 @@ public class MyFutureTask extends FutureTask {
     @Override
     protected void done() {
         try {
-            String dane = this.get().toString();
+            ArrayList<String> File = getFile();
+            File.set(getIndex(), File.get(getIndex())+this.get().toString());
+
             FileWriter f = null;
             lock.lock();
 
-            f = new FileWriter("C:\\Users\\mateu\\IdeaProjects\\Concurrent_file_read6\\src\\file.txt", true);
+            f = new FileWriter("C:\\Users\\mateu\\IdeaProjects\\Concurrent_file_read6\\src\\file.txt");
             BufferedWriter out = new BufferedWriter(f);
-            out.append(System.lineSeparator() + this.get().toString());
+            String temp = new String();
+            for(String e:File){
+
+                temp += e+System.lineSeparator();
+            }
+            out.write(temp);
+
             out.close();
 
             lock.unlock();
         }catch(Exception e){}
 
     }
+
+    public ArrayList<String> getFile() throws Exception{
+        ArrayList<String> contextFile = new ArrayList<String>();
+        FileInputStream fin;
+        {
+            try {
+                fin = new FileInputStream("C:\\Users\\mateu\\IdeaProjects\\Concurrent_file_read6\\src\\file.txt");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        DataInputStream in = new DataInputStream(fin);
+        BufferedReader r = new BufferedReader(new InputStreamReader(in));
+        String strLine = new String();
+        while((strLine = r.readLine()) != null){
+            contextFile.add(strLine);
+        }
+        return contextFile;
+    }
+
+    public int getIndex() throws Exception {
+
+        FileInputStream f;
+
+        {
+            try {
+                f = new FileInputStream("C:\\Users\\mateu\\IdeaProjects\\Concurrent_file_read6\\src\\file.txt");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        DataInputStream in = new DataInputStream(f);
+        BufferedReader r = new BufferedReader(new InputStreamReader(in));
+
+        String strLine = new String();
+        Boolean founded = false;
+        int foundedIndex = 0;
+        while ((strLine = r.readLine()) != null && !founded) {
+            strLine.trim();
+            int indexOfEquals = strLine.indexOf('=');
+            if(strLine.length() == indexOfEquals+1){
+                founded = true;
+            }else foundedIndex++;
+        }
+        in.close();
+        return foundedIndex;
+    }
+
 }
 
