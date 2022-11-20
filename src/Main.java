4a8d5849b2b78281ac1a9f238125f4a2a59bcd20
mateushzet <mateushzett@gmail.com>
2022-11-20 15:05:39 +0100

    nowy branch

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,19 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Scanner;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
+
 
         System.out.println("How many threads do you want to start?");
         Scanner sc = new Scanner(System.in);
         int threadsNumber = sc.nextInt();
         ExecutorService executor = null;
         MyFutureTask[] taskTab = new MyFutureTask[threadsNumber];
+
         for(int i = 0; i<threadsNumber; i++){
             executor = Executors.newFixedThreadPool(threadsNumber);
             taskTab[i] = new MyFutureTask (new CallableCalculator());
@@ -20,6 +24,8 @@ public class Main {
         for(int i = 0; i<threadsNumber; i++){
             executor.submit(taskTab[i]);
         }
+
+
         executor.shutdown();
         }
       }
