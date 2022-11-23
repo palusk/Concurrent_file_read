b672da9598953df4ae496fbaa607cab94f6a73ca
Kamil <k.planeta20@gmail.com>
2022-11-23 21:11:25 +0100

    return check

--- src/CallableCalculator.java
+++ src/CallableCalculator.java
@@ -1,14 +1,20 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class CallableCalculator implements Callable {
 
-    static ArrayList<Boolean> reservationLine = new ArrayList<Boolean>();
+    static LinkedList<Integer> reservationLine = new LinkedList<Integer>();
 
+    Lock lock = new ReentrantLock();
+    Condition txtWritten = lock.newCondition();
 
-   FileInputStream f;
+    FileInputStream f;
 
     {
         try {
@@ -19,7 +25,7 @@ public class CallableCalculator implements Callable {
     }
 
     DataInputStream in = new DataInputStream(f);
-   BufferedReader r = new BufferedReader(new InputStreamReader(in));
+    BufferedReader r = new BufferedReader(new InputStreamReader(in));
 
     public Object call() throws Exception {
         int lineNumber = 0;
@@ -29,20 +35,29 @@ public class CallableCalculator implements Callable {
         while ((strLine = r.readLine()) != null && !isCalculated) {
             strLine.trim();
             int indexOfEquals = strLine.indexOf('=');
-            if(strLine.length() == indexOfEquals+1){
 
-                reservationLine
-                strLine = strLine.replaceAll("=","");
-                ONP calculator = new ONP(strLine);
-                output += calculator.oblicz();
-                System.out.println(Thread.currentThread().getName() + "  -  "+ strLine);
-                isCalculated = true;
+            lock.lock();
+            boolean found = false;
+            for (int x : reservationLine) {
+                if (x == lineNumber) {
+                    found = true;
+                    break;
+                }
+                if (strLine.length() == indexOfEquals + 1 && found == false) {
+                    reservationLine.add(lineNumber);
+                    lock.unlock();
+
+                    strLine = strLine.replaceAll("=", "");
+                    ONP calculator = new ONP(strLine);
+                    output += calculator.oblicz();
+                    System.out.println(Thread.currentThread().getName() + "  -  " + strLine);
+                    isCalculated = true;
 
 
-            }else lineNumber++;
+                } else lineNumber++;
+            }
+            in.close();
+            return output;
         }
-        in.close();
-        return output;
-    }
 
-}
+    }
